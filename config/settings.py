080fe00ae85422432875d215716a29f2bae42286
mazzasaverio <saverio3107@gmail.com>
2024-01-11 13:19:26 +0100

    UPDATE

--- config/settings.py
+++ config/settings.py
@@ -1,22 +1,20 @@
+import os
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
-    environment: str = "development"
-
-    sqlalchemy_database_url_dev: str
-    sqlalchemy_database_url_prod: str
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_PORT: str = os.getenv("DB_PORT")
+    DB_NAME: str = os.getenv("DB_NAME")
+    DB_PASS: str = os.getenv("DB_PASS")
+    DB_USER: str = os.getenv("DB_USER")
 
     @property
     def sqlalchemy_database_url(self) -> str:
-        return (
-            self.sqlalchemy_database_url_dev
-            if self.environment == "development"
-            else self.sqlalchemy_database_url_prod
-        )
-
-    class Config:
-        env_file = ".env"
+        return f"postgresql://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
 
 settings = Settings()
