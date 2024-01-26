7c2e55a2074bc486c0890f2e2e29e5eec6c6bba0
mazzasaverio <saverio3107@gmail.com>
2024-01-26 11:02:45 +0100

    UPDATE

--- frontend/app/(auth)/(routes)/layout.tsx
+++
@@ -1,7 +0,0 @@
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="h-full flex items-center justify-center">{children}</div>
-  );
-};
-
-export default AuthLayout;
--- frontend/app/(dashboard)/(routes)/companies/page.tsx
+++
@@ -1,9 +0,0 @@
-const Companies = () => {
-  return (
-    <div>
-      <h1>Companies</h1>
-    </div>
-  );
-};
-
-export default Companies;
