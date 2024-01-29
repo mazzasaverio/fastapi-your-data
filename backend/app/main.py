0f379faa5a376e506c3728a1c820837765839196
mazzasaverio <saverio3107@gmail.com>
2024-01-29 14:26:57 +0100

    UPDATE

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,25 +1,29 @@
-from fastapi import FastAPI, Request, Depends
-from fastapi.responses import RedirectResponse, JSONResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from loguru import logger
-import sys
-from uuid import uuid4
 import uvicorn
 from starlette.middleware.base import BaseHTTPMiddleware
-
-# from backend.app.api.v1.endpoints.document import docs_router
 from app.api.v1.endpoints.company import router as company_router
-
 from fastapi.middleware.cors import CORSMiddleware
+from uuid import uuid4
+import sys
 
 app = FastAPI()
 
 
 class LoggingMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        logger.info(f"Incoming request: {request.method} {request.url}")
-        response = await call_next(request)
-        logger.info(f"Response status: {response.status_code}")
-        return response
+        request_id = str(uuid4())
+        logger.info(f"Request [{request_id}] start: {request.method} {request.url}")
+        try:
+            response = await call_next(request)
+            logger.info(
+                f"Request [{request_id}] completed: Status code {response.status_code}"
+            )
+            return response
+        except Exception as e:
+            logger.error(f"Request [{request_id}] failed: {e}")
+            raise e
 
 
 app.add_middleware(LoggingMiddleware)
@@ -30,40 +34,27 @@ app.include_router(company_router, prefix="/companies")
 # CORS middleware configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Specify domains if you want to restrict
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-# Metrics endpoint
 @app.get("/metrics")
 def metrics():
-    return {"message": "This is a dummy metrics endpoint."}
+    return {"message": "Metrics endpoint"}
 
 
-# Root endpoint
 @app.get("/")
 async def home():
     return RedirectResponse(url="/docs")
 
 
-# Logger configuration
-logger.remove()
-logger.add(
-    sys.stdout,
-    level="INFO",
-    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
-)
-logger.add(
-    "logs/esg_spider_{time}.log",
-    rotation="1 day",
-    retention="7 days",
-    level="INFO",
-    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
-)
-
+# # Logger configuration
+# logger.remove()
+# logger.add(sys.stdout, level="INFO", format="{time} | {level} | {message}")
+# logger.add("logs/app_{time}.log", rotation="1 day", retention="10 days", level="INFO")
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=8080, reload=True)
