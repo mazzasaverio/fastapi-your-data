a15d81e1b07c38a78a90125eab54dd5d5df41e8d
mazzasaverio <saverio3107@gmail.com>
2024-02-18 17:39:58 +0100

    UPDATE

--- backend/app/app/sub_app_1/core/config.py
+++ backend/app/app/sub_app_1/core/config.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 from typing import List
 import json
+from loguru import logger
+import sys
 
 load_dotenv()
 
@@ -17,11 +19,20 @@ class Settings(BaseSettings):
     DB_PASS: str = os.getenv("DB_PASS")
     DB_USER: str = os.getenv("DB_USER")
 
-    API_KEY: str = os.getenv("API_KEY")
+    DATABASE_URI: str = (
+        f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
 
     ASYNC_DATABASE_URI: str = (
         f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
+
+    # github module settings
+    GITHUB_ACCESS_TOKEN = os.getenv("GITHUB_ACCESS_TOKEN")
+
+    LOCATION = os.getenv("LOCATION", "Milan")
+    MAX_USERS = int(os.getenv("MAX_USERS", 10))
+    MAX_REPOS_PER_USER = int(os.getenv("MAX_REPOS_PER_USER", 5))
 
 
 settings = Settings()
