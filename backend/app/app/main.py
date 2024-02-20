aae3bef06efc52a5e9264c5caa5f02ab1099c4f3
mazzasaverio <saverio3107@gmail.com>
2024-02-20 09:36:53 +0100

    UPDATE

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -4,6 +4,8 @@ from contextlib import asynccontextmanager
 from app.database.init_db import init_db
 from fastapi.responses import RedirectResponse
 from fastapi_pagination import add_pagination
+import os
+import uvicorn
 
 
 @asynccontextmanager
@@ -27,3 +29,10 @@ def metrics():
 @app.get("/")
 async def home():
     return RedirectResponse(url="/docs")
+
+
+if __name__ == "__main__":
+
+    port = int(os.environ.get("PORT", 8080))
+
+    uvicorn.run(app, host="0.0.0.0", port=port)
